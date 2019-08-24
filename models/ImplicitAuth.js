bf772e3ed513a1748f9a50b6202882c51110fee2
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-24 16:44:41 +0530

    Add implicit auth  route and modal

---
+++ models/ImplicitAuth.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ImplicitAuthSchema = new Schema({
+  clientID: {
+    type: String,
+    require: true
+  },
+  applicationName: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = ImplicitAuth = mongoose.model(
+  'implicitAuth',
+  ImplicitAuthSchema
+);
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const { ErrorResponse, Validate } = require('../middleware');
+
+const ImplicitAuth = require('../models/ImplicitAuth');
+
+/**
+ * @description
+ * This method is used to create implicit auth entry
+ */
+router.post('/', async (req, res) => {
+  const { clientID, applicationName } = req.body;
+
+  const isValid = Validate([
+    { value: clientID, type: 'string', minLength: 3 },
+    { value: applicationName, type: 'string', minLength: 3 }
+  ]);
+
+  if (!isValid) {
+    return res.status(404).json({ message: 'Invalid arugments sent' });
+  }
+
+  try {
+    const client = await ImplicitAuth.findOne({ clientID });
+
+    if (client) {
+      res.status(404).json({ message: 'ClientID already exist' });
+    }
+
+    const newOpenID = new ImplicitAuth({ clientID, applicationName });
+    await newOpenID.save();
+
+    return res.status(200).json({ message: 'Created Open id succesfully' });
+  } catch (error) {
+    return ErrorResponse(res, error);
+  }
+});
+
+module.exports = router;
