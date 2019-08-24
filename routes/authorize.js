b02a0f0e092fa4a99b53852a2fa24c442b035e1f
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-24 17:14:58 +0530

    Added client id validation

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const ImplicitAuth = require('../models/ImplicitAuth');
+const { ErrorHandler } = require('../middleware');
+
 /**
  * @description
  * This function is used to redirect user to webpage where user can login and get authenicated
@@ -11,9 +14,20 @@ const router = express.Router();
  *
  * @route /authorize?clientID=<<string>>&redirectURI=<<string>>
  */
-router.get('/', (req, res) => {
-  const reqQuery = req.query;
-  res.json(reqQuery);
+router.get('/', async (req, res) => {
+  const { clientID } = req.query;
+
+  try {
+    const client = await ImplicitAuth.findOne({ clientID });
+
+    if (!client) {
+      return res.status(404).json({ message: 'Invalid Client ID' });
+    }
+
+    return res.json(client);
+  } catch (error) {
+    return ErrorHandler(res, error);
+  }
 });
 
 module.exports = router;
