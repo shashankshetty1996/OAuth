76e6b89df726c30c8e8205b9365b3fd08e42374b
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-24 16:43:23 +0530

    Add config file

---
+++ config/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('MONGO_URI');
+
+const connectDB = async () => {
+  try {
+    // Mapping global promise with mongoose
+    mongoose.Promise = global.Promise;
+
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
