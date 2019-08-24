e188492cfc78456160f8b362b352c686a94d326d
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-24 15:06:33 +0530

    Add authorized route

---
+++ routes/authorize.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @description
+ * This function is used to redirect user to webpage where user can login and get authenicated
+ *
+ * @access PUBLIC
+ *
+ * @method GET
+ *
+ * @route authorize?clientID=<<string>>&redirectURI=<<string>>
+ */
+router.get('/', (req, res) => {
+  const reqQuery = req.query;
+  res.json(reqQuery);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const path = require('path');
 
 const app = express();
 
+/**
+ * @description
+ * This route is used to for server health check
+ *
+ * @access PUBLIC
+ *
+ * @method GET
+ *
+ * @route '/'
+ */
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'server-running.html'));
 });
 
 app.use('/user', require('./routes/user'));
+app.use('/authorize', require('./routes/authorize'));
 
 const PORT = process.env.PORT || config.get('PORT') || 5000;
 
