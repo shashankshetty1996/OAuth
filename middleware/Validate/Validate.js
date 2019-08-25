8d111bc0978a8373a40e58b8e04a1f67f51b6eb3
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-25 17:10:51 +0530

    Add isValidValue validation

--- middleware/Validate/Validate.js
+++ middleware/Validate/Validate.js
@@ -13,21 +13,56 @@ function validateEmail(value) {
   return new RegExp(/^(.+)@(.+){2,}\.(.+){2,}$/g).test(value);
 }
 
+function isValidValue(value, type) {
+  if (type !== 'array' && typeof value !== type) {
+    return false;
+  } else if (
+    type === 'array' &&
+    (typeof value !== 'object' || !Array.isArray(value))
+  ) {
+    return false;
+  } else {
+    switch (typeof value) {
+      case 'string': {
+        return value !== '';
+      }
+
+      case 'number': {
+        return !isNaN(value);
+      }
+
+      case 'object': {
+        if (Array.isArray(value)) {
+          return value.length !== 0;
+        } else {
+          return Object.keys(value).length !== 0;
+        }
+      }
+
+      default:
+        return true;
+    }
+  }
+}
+
 function isValueValid(value, options) {
-  if (!value) return false;
   if (!options) return new Error('Invalid options');
 
   const { type, maxLength, minLength } = options;
 
-  switch (type) {
-    case 'number':
-      return validateNumber(value);
-    case 'string':
-      return validateString(value, maxLength, minLength);
-    case 'email':
-      return validateEmail(value);
-    default:
-      return false;
+  if (isValidValue(value, type)) {
+    switch (type) {
+      case 'number':
+        return validateNumber(value);
+      case 'string':
+        return validateString(value, maxLength, minLength);
+      case 'email':
+        return validateEmail(value);
+      default:
+        return false;
+    }
+  } else {
+    return false;
   }
 }
 
