242b910e9e1ebaa167cc3e7b7e75f8bd0929e9ef
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-25 14:25:49 +0530

    Add login UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
 // JS code here
+(function() {
+  const clearValue = $event =>
+    ($event.target.previousElementSibling.value = '');
+
+  // Selecting all clear-value button then attaching clear input field event listner.
+  document
+    .querySelectorAll('.clear-value')
+    .forEach(btn => btn.addEventListener('click', clearValue));
+})();
