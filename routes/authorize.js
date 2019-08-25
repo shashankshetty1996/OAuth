f3cf7ce13696acebe6f36e5e82fb31625565b1a4
smile.shashank.s.shetty1996@gmail.com <shashank.shetty@ymedialabs.com>
2019-08-25 15:17:43 +0530

    Authorization basic login done

--- routes/authorize.js
+++ routes/authorize.js
@@ -18,15 +18,56 @@ router.get('/', async (req, res) => {
   const { clientID, redirectURI } = req.query;
 
   try {
-    const client = await ImplicitAuth.findOne({ clientID });
+    // Validate client
+    const client = await ImplicitAuth.findOne({ clientID, redirectURI });
 
     if (!client) {
-      return res.status(404).json({ message: 'Invalid Client ID' });
+      return res
+        .status(404)
+        .json({ message: 'Invalid Client ID or redirectURI' });
     }
+
+    // Redirect to login form
     return res.render('login', {
-      clientID,
-      redirectURI
+      id: client._id
     });
+  } catch (error) {
+    return ErrorHandler(res, error);
+  }
+});
+
+/**
+ * @description
+ * This methos is used to authenicate authorized user.
+ *
+ * @method POST
+ *
+ * @access Protected
+ *
+ * @route /authorize/id<<ObjectId>>
+ */
+router.post('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { email, password } = req.body;
+
+  // Validation of id
+  if (!id) {
+    return res.status(404).json({ message: `Client id hasn't been passed` });
+  }
+
+  try {
+    // Validate client
+    const client = await ImplicitAuth.findById(id);
+
+    if (!client) {
+      return res.status(403).json({ message: `Application isn't registered` });
+    }
+
+    // Do user Authenication here
+    const accessToken = 'Take your accessToken from here';
+
+    // redirect to RedirectURI
+    return res.redirect(`${client.redirectURI}?accessToken=${accessToken}`);
   } catch (error) {
     return ErrorHandler(res, error);
   }
